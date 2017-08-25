00ce20b6c9309f14e21bc7c9212a4dba70fad625
Devilie <devilie1989@gmail.com>
2017-08-25 05:41:51 +0300

    Add: authentication; modal window fo asc question.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,58 @@
-'use strict';
+"use strict";
 
 window.onload = getUserName;
 var userNameId;
+const uIdKey = "username";
+var usernameDiv = document.getElementsByClassName("user-id-name")[0];
+var getNameId = document.getElementsByClassName("get-username-modal")[0];
+var usernInpt = document.getElementsByClassName("user-n-id")[0];
+var usernameModalBtn = document.getElementsByClassName("modal-username-btn")[0];
+var allQHolder = document.getElementsByClassName("all-q")[0];
+var allAnsHolder = document.getElementsByClassName("all-ans")[0];
+var noAnsHolder = document.getElementsByClassName("no-asw")[0];
+var ascQ = document.getElementsByClassName("asc-q")[0];
+var ascQModal = document.getElementsByClassName("asc-q-modal")[0];
+
+ascQ.addEventListener("click",addQuestion);
+
+function addQuestion(){
+    ascQModal.removeAttribute("style");
+}
+
 function getUserName() {
-    // Добавить проверку на наличие username в local storage; 
     // по нажатию кнопки доставать введенный пользователем логин и подставлять в usernameDiv
-    var UsernameModalBtn = document.getElementsByClassName("modal-username-btn")[0].addEventListener("click", verifideUsername);
-    //<button type="button" onClick="document.getElementsByClassName('get-username-modal')[0].style.display='none';">Сохранить</button>
-    userNameId = document.getElementsByClassName('get-username-modal')[0].removeAttribute('style');
-    var usernameDiv = document.getElementsByClassName("user-id-name");
-    usernameDiv[0].innerHTML = "Somebody";
+
+    if (localStorage.getItem(uIdKey)) {
+        userNameId = localStorage.getItem(uIdKey);
+        setUsername();
+    } else {
+        getNameId.removeAttribute("style"); //отображение модального окна -> запрос юзернейма
+        usernInpt.addEventListener("input", inputFormated);
+        usernameModalBtn.addEventListener("click", setUsername); //отлавливаем нажатие кнопки сохранения
+    }
+
+}
+
+function setUsername() {
+    if (usernInpt.value.length >= 3 || userNameId) {
+        if (!userNameId) {
+            userNameId = usernInpt.value;
+            getNameId.style.display = "none";
+            if (!localStorage.getItem(uIdKey)) {
+                localStorage.setItem(uIdKey, userNameId);
+            }
+        }
+        usernameDiv.innerHTML = userNameId;
+    } else if (userNameId) {} else {
+        usernInpt.focus();
+    }
+}
+
+function inputFormated() {
+    usernInpt.value = formatStr(usernInpt.value);
+}
+
+function formatStr(str) {
+    str = str.trim().replace(/\s+/g, " ").replace(/\n+/g, " ");
+    return str;
 }
